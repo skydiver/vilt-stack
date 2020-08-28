d0cc27ba532a05fb936b7165eb8eef2001f56a7e
Martín M <skydiver@users.noreply.github.com>
2020-08-27 22:00:01 -0300

    tailwind css + tailwind ui

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,20 @@
-const mix = require('laravel-mix');
 const path = require('path');
+const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 mix
   .js('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css')
+  .options({
+    processCssUrls: false,
+    postCss: [tailwindcss('./tailwind.config.js')]
+  })
   .webpackConfig({
     output: { chunkFilename: 'js/[name].js?id=[chunkhash]' },
     resolve: {
       alias: {
         vue$: 'vue/dist/vue.runtime.esm.js',
-        '@': path.resolve('resources/js')
+        '~': path.resolve('resources/js')
       }
     }
   })
