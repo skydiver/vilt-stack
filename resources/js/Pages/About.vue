74a57b1f97666ca98fcdbd96aaef1728fe18959f
Martín M <skydiver@users.noreply.github.com>
2020-08-27 21:45:22 -0300

    sample pages + layout

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <layout>
+    <h1 class="text-2xl font-semibold mb-3">About</h1>
+    <p>About my first Inertia.js app!</p>
+  </layout>
+</template>
+
+<script>
+import Layout from '@/Shared/Layout'
+
+export default {
+  components: {
+    Layout,
+  },
+}
+</script>
---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <navbar />
+
+        <main>
+            <div class="w-full max-w-6xl mx-auto p-4">
+                <article>
+                    <slot />
+                </article>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+import Navbar from '@/Shared/Navbar'
+
+export default {
+     components: {
+         Navbar,
+     }
+}
+</script>
