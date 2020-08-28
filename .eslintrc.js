82a8493035f7f1de9dcbe03689a1015fc2846bc6
Martín M <skydiver@users.noreply.github.com>
2020-08-27 21:50:32 -0300

    prettier + eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/essential",
+    "plugin:prettier/recommended",
+    "eslint:recommended"
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+}
